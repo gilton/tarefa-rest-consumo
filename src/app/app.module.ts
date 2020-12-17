fa5253c37dffb423c7f03807b614ae196fb1229b
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-12-17 19:07:07 -0300

    fix: retirando referencias nao usadas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CampeonatoService } from './services/campeonato.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CensoComponent } from './components/censo/censo.component';
 import { CampeonatosComponent } from './components/campeonatos/campeonatos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CensoComponent,
     CampeonatosComponent
   ],
   imports: [
